4543b1e26b5e705049b00998de4c328c413d2ef6
wrunion <winterrunion@gmail.com>
2020-05-03 14:03:13 -0700

    Add all tests for reducers.

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,3 +1,16 @@
-export default (state ={}, action) => {
-  return state;
+export default (state = {}, action) => {
+  switch (action.type) {
+  case 'ADD_TICKET':
+    const { names, location, issue, id } = action;
+    return Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
